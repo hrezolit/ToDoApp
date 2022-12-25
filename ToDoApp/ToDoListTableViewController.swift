467d8505231f3e37e1e763c9660004e1b5542234
Nik <hrezolitaz@gmail.com>
2022-12-25 10:13:29 +0600

    add action button for adding new items in table view add alert controller for action button

--- ToDoApp/ToDoListTableViewController.swift
+++ ToDoApp/ToDoListTableViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ToDoListTableViewController: UITableViewController {
     
-    var mockData = ["Buy vegetables", "Make comunal payment", "Buy something for wife", "Drink Yogurt"]
+    var toDoItems = ["Buy vegetables", "Make comunal payment", "Buy something for wife", "Drink Yogurt"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +23,23 @@ class ToDoListTableViewController: UITableViewController {
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Add", style: .default) { action in
+            guard let text = textField.text else { return }
+            self.toDoItems.append(text)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { alertTF in
+            alertTF.placeholder = "Write new item here"
+            textField = alertTF
+        }
+        
+        alert.addAction(alertAction)
+        
+        present(alert, animated: true)
         
     }
     
@@ -30,14 +47,14 @@ class ToDoListTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return mockData.count
+        return toDoItems.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
         var configuration = cell.defaultContentConfiguration()
-        configuration.text = mockData[indexPath.row]
+        configuration.text = toDoItems[indexPath.row]
         
         cell.contentConfiguration = configuration
         
