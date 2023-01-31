42a35e725a41577f446e47f302af894b5c97d3a0
Nik <hrezolitaz@gmail.com>
2023-01-31 10:50:53 +0600

    add data source methods add methods for saving/loading data

--- ToDoApp/Controller/ToDoListTableViewController.swift
+++ ToDoApp/Controller/ToDoListTableViewController.swift
@@ -10,60 +10,104 @@ import CoreData
 
 final class ToDoListTableViewController: UITableViewController {
     
-    var itemArray = [Item]()
-    let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
-    
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    private var itemArray = [Item]()
+    private let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
     
     //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Data base file path
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist") as Any)
+        
+        loadData()
     }
     
     //MARK: - actions
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        addAlertController(title: "Add new item", buttonTitle: "Add", placeholderText: "Write new item here")
+    }
+    
+    /// Creat alert controller
+    /// - Parameters:
+    ///   - title: title for alert controller
+    ///   - buttonTitle: tittle for button
+    ///   - placeholderText: text for placeholder
+    private func addAlertController(title: String, buttonTitle: String, placeholderText: String) {
         var textField = UITextField()
         
         // Alert controller
-        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
-        let alertAction = UIAlertAction(title: "Add", style: .default) { [weak self] action in
+        let alert = UIAlertController(title: title, message: "", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: buttonTitle, style: .default) { [weak self] action in
+            
+            // Add data for saving
             guard let context = self?.context else { return }
             let newItem = Item(context: context)
+            
             newItem.title = textField.text
             newItem.done = false
+            
             self?.itemArray.append(newItem)
-            self?.saveItems()
+            self?.saveData()
         }
         
         alert.addTextField { alertTF in
-            alertTF.placeholder = "Write new item here"
+            alertTF.placeholder = placeholderText
             textField = alertTF
         }
         
         alert.addAction(alertAction)
         
         present(alert, animated: true)
-        
     }
     
-    /// Saving data using property list encoder
-    private func saveItems() {
+    // MARK: - Methods for Core Data manipulations:
+    
+    /// Saving data
+    private func saveData() {
         
         do {
             try context?.save()
         } catch {
             print(String(describing: error))
         }
-        
         self.tableView.reloadData()
     }
     
+    /// Loading data
+    private func loadData() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            guard let context else { return }
+            itemArray = try context.fetch(request)
+        } catch {
+            print(String(describing: error))
+        }
+    }
     
     // MARK: - numberOfRowsInSection
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
+    }
+    
+    // MARK: - edit for row
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            context?.delete(itemArray[indexPath.row])
+            itemArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            saveData()
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
+    // MARK: - titleForDeleteConfirmationButtonForRowAt
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Remove"
     }
     
     // MARK: - cellForRowAt
@@ -92,6 +136,6 @@ final class ToDoListTableViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
         
-        saveItems()
+        saveData()
     }
 }
